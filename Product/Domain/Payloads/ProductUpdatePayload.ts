d13d789a208c3e86f00cfa6ef7c8eaeb54b5f282
Gabriel Tomasel <84481967+GTomasel@users.noreply.github.com>
2022-12-26 15:04:25 -0300

    commit

--- Product/Domain/Payloads/ProductUpdatePayload.ts
+++
@@ -1,7 +0,0 @@
-import IdPayload from '../../../Shared/Presentation/Requests/IdPayload';
-import ProductRepPayload from './ProductRepPayload';
-import IUserDomain from '../../../Auth/Domain/Entities/IUserDomain';
-
-interface ProductUpdatePayload extends IdPayload, ProductRepPayload {}
-
-export default ProductUpdatePayload;
--- Product/Infrastructure/Schemas/ProductMongooseDocument.ts
+++
@@ -1,6 +0,0 @@
-import { Document } from 'mongoose';
-import IProductDomain from '../../Domain/Entities/IProductDomain';
-
-type ProductMongooseDocument = Document & IProductDomain
-
-export default ProductMongooseDocument;
