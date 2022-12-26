0b1b42706fdb16cd90d28f143a7a435b2377a615
Gabriel Tomasel <84481967+GTomasel@users.noreply.github.com>
2022-12-26 15:04:11 -0300

    commit

--- Category/Domain/Payloads/CategoryUpdatePayload.ts
+++
@@ -1,7 +0,0 @@
-import IdPayload from '../../../Shared/Presentation/Requests/IdPayload';
-import CategoryRepPayload from './CategoryRepPayload';
-import IUserDomain from '../../../Auth/Domain/Entities/IUserDomain';
-
-interface CategoryUpdatePayload extends IdPayload, CategoryRepPayload {}
-
-export default CategoryUpdatePayload;
--- Category/Infrastructure/Schemas/CategoryMongooseDocument.ts
+++
@@ -1,6 +0,0 @@
-import { Document } from 'mongoose';
-import ICategoryDomain from '../../Domain/Entities/ICategoryDomain';
-
-type CategoryMongooseDocument = Document & ICategoryDomain
-
-export default CategoryMongooseDocument;
